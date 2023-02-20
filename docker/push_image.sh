88d1bbdac30e53c6079d101b46610bb95aa10fe4
Terry Lin <terry@fstnjp.com>
2023-02-20 12:17:28 +0000

    Add frontend docker scirpt

---
+++ docker/push_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+TAG=$1
+
+if [ "$#" -ne 1 ]; then
+	TAG="latest"
+fi
+
+GITUSER=257042641977.dkr.ecr.us-west-2.amazonaws.com
+GITREPO=substrate-telemetry-frontend
+
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 257042641977.dkr.ecr.us-west-2.amazonaws.com
+
+echo Push $GITUSER/$GITREPO:$TAG
+docker push $GITUSER/$GITREPO:$TAG
